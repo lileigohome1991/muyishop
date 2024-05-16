730c4685e5548e07258e280101ee5f7a24b032ef
limuyi666 <1846352100@qq.com>
2024-05-16 15:43:07 +0000

    基础布局

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/test', function () {
-    return "666";
-});
+Route::get('/', 'PagesController@root')->name('root');
